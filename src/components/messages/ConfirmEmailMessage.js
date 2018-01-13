ff8bd79d45c6b2c72e735356171e5ce7d9d44f6c
Pietro Lazzarotto <pietro.lazz@gmail.com>
2018-01-13 13:23:42 +0000

    Authorised routes

---
+++ src/components/messages/ConfirmEmailMessage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Message } from "semantic-ui-react";
+
+const ConfirmEmailMessage = (props) => (
+	<Message info>
+		<Message.Header>
+		Please, Verify your email to unlock awesomeness
+		</Message.Header>
+	</Message>
+);
+
+export default ConfirmEmailMessage
